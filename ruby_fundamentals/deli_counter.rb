ad384b1a4e823f1d7fe5a49ae46b61eaeb4f812a
Hain Thit <hainlthit@gmail.com>
2022-01-31 15:04:47 -0500

    commit tree 3b4b2cdf616b359824705e1062bbcd3f97f4df14

--- ruby_fundamentals/deli_counter.rb
+++ ruby_fundamentals/deli_counter.rb
@@ -1,15 +1,19 @@
 # Write your code here.
 
-def line(arr)
-    if arr.empty?
-        puts "The line is currently empty."
-    else
-        count = 1
-        string = "The line is currently:"
-        arr.each do |person|
-            string << " #{count}. #{person}"
-            count += 1
-        end
-        puts string
-    end
+# def line(arr)
+#     if arr.empty?
+#         puts "The line is currently empty."
+#     else
+#         count = 1
+#         string = "The line is currently:"
+#         arr.each do |person|
+#             string << " #{count}. #{person}"
+#             count += 1
+#         end
+#         puts string
+#     end
+# end
+
+def Line 
+
 end
